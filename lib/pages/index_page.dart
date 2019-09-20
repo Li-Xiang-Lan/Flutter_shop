f9a850c3d422c72cffb6e7e73a9a83514a532351
chengmengyi <chengmengyi@cdxiangxue.com.cn>
2019-09-20 17:31:52 +0800

    first commit

---
+++ lib/pages/index_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import '../pages/tabs/home_page.dart';
+import '../pages/tabs/category_page.dart';
+import '../pages/tabs/car_page.dart';
+import '../pages/tabs/mine_page.dart';
+
+class IndexPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _IndexPageState();
+  }
+}
+
+class _IndexPageState extends State<IndexPage>{
+  final List<BottomNavigationBarItem> bottomTabs=[
+    BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        title: Text("首页")
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.category),
+        title: Text("分类")
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.shopping_cart),
+        title: Text("购物车")
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.people),
+        title: Text("我的")
+    ),
+  ];
+
+  final List<Widget> tabBodies=[
+    HomePage(),
+    CategoryPage(),
+    CarPage(),
+    MinePage()
+  ];
+
+  var currentIndex=0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: bottomTabs,
+        currentIndex: currentIndex,
+        onTap: (v){
+          setState(() {
+            this.currentIndex=v;
+          });
+        },
+      ),
+      body: tabBodies[currentIndex],
+    );
+  }
+}
---
+++ lib/pages/tabs/mine_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MinePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(child: Text("我的"),);
+  }
+}
